e067efd7003d7f71c2e369c576edc1c891aedff1
Pixel Vega <pixelvega@Pixels-MacBook-Pro.local>
2019-01-23 15:34:43 +0100

    github pages

---
+++ docs/precache-manifest.2d5817299e20e2fd74e4963cd4595166.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31482f566bf5a587b0a5",
+    "url": "/static/js/main.31482f56.chunk.js"
+  },
+  {
+    "revision": "fb14998714f85a0181a8",
+    "url": "/static/js/1.fb149987.chunk.js"
+  },
+  {
+    "revision": "31482f566bf5a587b0a5",
+    "url": "/static/css/main.0bbfb2f4.chunk.css"
+  },
+  {
+    "revision": "6e8cbaaf3a3d60fb351fb657df4b3a79",
+    "url": "/index.html"
+  }
+];
